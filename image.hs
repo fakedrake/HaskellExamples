3ea6b6573b21917fce43b373b4c58f93934b8b25
Chris Perivolaropoulos <cperivol@csail.mit.edu>
2015-12-08 01:56:55 +0200

    Image editing nonsense

---
+++ image.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+import Prelude hiding (filter)
+import System.Environment (getArgs)
+import System.Directory
+import Control.Monad (when)
+
+import Vision.Detector.Edge (canny)
+import Vision.Image
+import Vision.Image.Storage.DevIL (Autodetect (..), load, save)
+
+-- Detects the edge of the image with the Canny's edge detector.
+--
+-- usage: ./canny input.png output.png
+main :: IO ()
+main = do
+    [input, output] <- getArgs
+
+    -- Loads the image. Automatically infers the format.
+    io <- load Autodetect input
+    putStrLn "Input:"
+    putStrLn input
+    putStrLn "Output:"
+    putStrLn output
+
+    case io of
+        Left err             -> do
+            putStrLn "Unable to load the image:"
+            print err
+        Right (grey :: Grey) -> do
+            let blurred, edges :: Grey
+                -- Applies a Gaussian filter with a 3x3 Double kernel to remove
+                -- small noises.
+                blurred = gaussianBlur 1 (Nothing :: Maybe Double) grey
+
+                -- Applies the Canny's algorithm with a 5x5 Sobel kernel (radius
+                -- = 2).
+                edges = canny 2 2 3 blurred
+
+            -- Saves the edges image. Automatically infers the output format.
+            exists <- doesFileExist output
+            when exists (removeFile output)
+            mErr <- save Autodetect output edges
+            case mErr of
+                Nothing  ->
+                    putStrLn "Success."
+                Just err -> do
+                    putStrLn "Unable to save the image:"
+                    print err
